89844e19da60f31fde8bf2febce0a0be659500cb
JJohnsonChan <asunmoon25@gmail.com>
2018-11-22 15:44:22 +0800

    first push

---
+++ AVFRTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFRTest
+//
+//  Created by Johnson Chan on 2018/11/22.
+//  Copyright © 2018 Johnson Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
